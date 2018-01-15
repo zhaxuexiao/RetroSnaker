7488a95ec9bdf0211d48c8696f5f2cb1976edc3f
zhaxuexiao <276873780@qq.com>
2018-01-15 16:43:42 +0800

    补充提交：贪吃蛇DEMO碰撞监测

---
+++ app/src/main/java/com/ne/www/snaker/SnakerBody.java
@@ -0,0 +1,18 @@
+package com.ne.www.snaker;
+
+/**
+ * Created by OOXX on 18/1/15.
+ */
+
+public class SnakerBody {
+    public int x = 0;
+    public int y = 0;
+
+    @Override
+    public String toString() {
+        return "SnakerBody{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
